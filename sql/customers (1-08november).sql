519511c2f4b6b7307c37a7a5e51e7991020a8564
IrynaMe <irinam7a@gmail.com>
2022-11-08 12:23:35 +0100

    added sql/customers(1-08novenìmber).sql

---
+++ sql/customers (1-08november).sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 08, 2022 at 11:21 AM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Nome',
+  `cognome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Cognome',
+  `indirizzo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Indirizzo',
+  `citta` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Roma',
+  `nazione` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Italia',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `email`, `password`, `nome`, `cognome`, `indirizzo`, `citta`, `nazione`, `created_at`, `updated_at`) VALUES
+(1, 'admin@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Iryna', 'Melnikava', 'piazza Duomo', 'Roma', 'italia', '2022-11-04 08:21:48', '2022-11-04 08:21:48'),
+(2, 'admin2@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Nice', 'Surname', 'via Limone', 'Roma', 'Italia', NULL, NULL),
+(3, 'admin3@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Admin3', 'Admin3', 'Via Papaya', 'Roma', 'Italia', NULL, '2022-11-08 07:05:50'),
+(5, 'admin4@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Nome', 'Cognome', 'Indirizzo', 'Roma', 'Italia', '2022-11-08 08:42:29', '2022-11-08 08:42:29'),
+(6, 'Admin5@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Nome', 'Cognome', 'Indirizzo', 'Roma', 'Italia', '2022-11-08 08:42:52', '2022-11-08 08:42:52'),
+(7, 'admin6@test.it', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', 'Nome', 'Cognome', 'Indirizzo', 'Roma', 'Italia', '2022-11-08 08:43:36', '2022-11-08 08:43:36');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
